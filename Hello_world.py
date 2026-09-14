dcc945d765d2d601495e7b6cc7c0e2093d814a53
Eric Cloutier <ecloutier1@etu.uqac.ca>
2026-09-14 18:46:31 -0400

    adding hello world.py

---
+++ Hello_world.py
@@ -0,0 +1,19 @@
+import sys
+
+a = "je suis un test string"
+print(sys.argv[1])
+
+
+
+# NOTE POUR TP1 +/- 180 lignes
+# Comment charger un fichier json en mémoire
+this_is_a_variable = 42 # Ok
+thisIsAVariable = 42 # Wrong
+
+#def = pour définir une fonction, my_function = nom de la fonction, () = paramètres de la fonction
+def my_function(): # Ok
+    print("Good Function Name")
+def myFunction(): # Wrong
+    print("Bad Function Name")
+
+my_function("Will", 1.2, [420])
