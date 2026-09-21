e94cb9c9d827d484c6fc48082598dcc609d921ee
EricClout <ecloutier1@etu.uqac.ca>
2026-09-21 17:40:21 -0400

    Add Atelier #1

---
+++ Atelier/#1/Atelier_1.py
@@ -0,0 +1,21 @@
+# Note Perso: 
+# sélectionner plusieurs lignes CTRL + ALT + SHIFT
+# input c'est une fonction avec paremètre type string  
+# Façon de setter les strings:
+# f string, f("bla bla bla")
+# ajouter un + variable, "bla bla bla" + variable
+# avec simple guillemet ' ' + variable, 'bla bla bla' + variable
+
+
+
+# Demander d'entrer le nom
+# print("Qui êtes-vous ?")
+
+# Lire l'input
+name = input("Qui êtes-vous? ")
+
+# Greetings
+print(f"Bienvenue à Cactusity, {name}")
+
+ville = input("D'où viens-tu mon ami? ")
+print(f"Ah, tu viens de {ville}! C'est un endroit magnifique.")
